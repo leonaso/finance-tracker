d1f5f0739783131344208cc507b4bde7d3da3122
lscloud <leonar_soto@yahoo.com>
2017-02-10 18:46:22 +0000

    Add stocks search and friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users, :controllers => { :registrations => "users/registrations" }
+  #devise_for :users, :controllers => { :registrations => "user/registrations" }
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks
+  resources :users
+  resources :friendships
+  #resources :user_stocks, except: [:show, :edit, :update]
   # get 'welcome/index'
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
+  get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: "users#search"
+  post 'add_friend', to: "users#add_friend"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
